2ea1848e7a1e043aa6668c4eb1074396164f6989
Brgraul <brgraul@gmail.com>
2021-01-17 20:01:07 +0100

    First commit

--- express/server.js
+++ express/server.js
@@ -7,9 +7,7 @@ const bodyParser = require('body-parser');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
-  res.write('<h1>Hello from Express.js!</h1>');
-  res.end();
+  res.redirect('https://www.pushquantum.tech/qel')
 });
 router.get('/another', (req, res) => res.json({ route: req.originalUrl }));
 router.post('/', (req, res) => res.json({ postBody: req.body }));
@@ -20,3 +18,4 @@ app.use('/', (req, res) => res.sendFile(path.join(__dirname, '../index.html')));
 
 module.exports = app;
 module.exports.handler = serverless(app);
+
